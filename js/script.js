6dc875dd98e24bd995030fb5eb229e39445b8689
Daniel <danny.garcia80@hotmail.com>
2018-12-17 09:32:56 -0800

    will edit starter comments

--- js/script.js
+++ js/script.js
@@ -9,12 +9,6 @@ FSJS project 2 - List Filter and Pagination
 /***
    Add your global variables that store the DOM elements you will
    need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
 ***/
 const students = document.querySelectorAll(".student-item"); /*** select the student item class ***/
 const maxStudents = 10; /*** will make maximum 10 students per page ***/
@@ -26,16 +20,6 @@ let pageNumber = 1; /*** start the page at page 1 ***/
 /***
    Create the `showPage` function to hide all of the items in the
    list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function.
 ***/
 const showPage = (list, page) => {
 
